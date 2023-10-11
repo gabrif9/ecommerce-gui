f1d21b9cfe306d188f7b4f182a4e9081298fb1a1
gabrif9 <g.ferrigno@studenti.unipi.it>
2023-10-11 18:06:24 +0200

    feat: added filter items in the homepagecomponent

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { Product, ProductList } from 'src/app/module/product.module';
 import { ProductManagementServiceService } from 'src/app/services/product-management-service.service';
 
@@ -10,11 +10,34 @@ import { ProductManagementServiceService } from 'src/app/services/product-manage
 export class HomePageComponent {
 
   products?: ProductList[]
+  productsBackup?: ProductList[]
+
+  categories: string[] = ["men's clothing", "jewelery", "electronics", "women's clothing"]
+
+  @ViewChildren("checkboxes") checkboxes?: QueryList<ElementRef>
 
   constructor(private productManagementService: ProductManagementServiceService) {
     this.productManagementService.getProducts().subscribe(item => {
       this.products = (item as Product).products
-      console.log(this.products)
+      this.productsBackup = this.products
     })
+  }
+
+  filterItems(category: string) {
+    var tmpProducts = this.products?.filter(item => {
+      return item.category === category
+    })
+    this.products = tmpProducts
+
+    var atLeastOneChecked = false;
+    this.checkboxes!.forEach((element) => {
+      if(element.nativeElement.checked){
+        atLeastOneChecked = true
+      }
+    })
+
+    if(!atLeastOneChecked) {
+      this.products = this.productsBackup
+    }
   }
 }
